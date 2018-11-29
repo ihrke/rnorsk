70df09f59936749593d9af920f96d51ea042c743
Matthias Mittner <matthias.mittner@uit.no>
2018-11-29 13:30:41 +0100

    some initialization

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Number of new students in Norway by year, age and gender
+#'
+#' A dataset containing the number of new students in Norway by year, age and gender for
+#' the years 2000-2017. The data was downloaded from Det Statistiske Sentralbyrå (Statistics
+#' Norway) https://www.ssb.no/.
+#'
+#'
+#' @format A data frame with 504 rows and 4 variables:
+#' \describe{
+#'   \item{kjønn}{Gender (one of "Menn"-male or "Kvinner"-female)}
+#'   \item{alder}{agegroup (factor)}
+#'   \item{år}{year}
+#'   \item{studenter}{number of new students}
+#' }
+#' @source \url{https://www.ssb.no/}
+"nyeStudenter"
+
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# rnorsk
+
+The goal of rnorsk is to ...
+
+## Installation
+
+You can install the released version of rnorsk from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("rnorsk")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+## basic example code
+```
+
