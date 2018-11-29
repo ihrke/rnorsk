cb1157d217a1019deef2934a1b7cfd3ea0d63cac
Matthias Mittner <matthias.mittner@uit.no>
2018-11-29 14:29:38 +0100

    use only the reduced dataset nyeStudenter

--- R/data.R
+++ R/data.R
@@ -8,8 +8,7 @@
 #' @format A data frame with 504 rows and 4 variables:
 #' \describe{
 #'   \item{kjønn}{Gender (one of "Menn"-male or "Kvinner"-female)}
-#'   \item{alder}{agegroup, coded are beginning student <18, each year from 19-29 separately, 30-34 and >35}
-#'   \item{alder_grov}{agegroup, coded are only <18, 19-29 and >30}
+#'   \item{alder}{agegroup, coded are <18, 19-29 and >30}
 #'   \item{år}{year}
 #'   \item{studenter}{number of new students}
 #' }
