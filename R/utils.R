94d5d40f4c34ee8dfc614bd2888de2ba27680710
Matthias Mittner <matthias.mittner@uit.no>
2019-10-11 13:20:35 +0200

    hex sticker for function

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,22 @@ create_rnorsk_hex <- function(){
           h_fill="#ef2b2d",
           filename = "inst/extdata/rnorskhex.png")
 }
+create_package_hex <- function(){
+  hexSticker::sticker("inst/extdata/package.png",
+                      package="",
+                      p_size=12, s_x=1, s_y=.95, s_width=.6,
+                      p_y=1,
+                      h_color="#000000",
+                      h_fill="#ef2b2d55",
+                      filename = "inst/extdata/packagehex.png")
+}
+
+create_function_hex <- function(){
+  hexSticker::sticker("inst/extdata/wheel.png",
+                      package="function()",
+                      p_size=8, s_x=1, s_y=.95, s_width=.6,
+                      p_y=1,
+                      h_color="#000000",
+                      h_fill="#043e9b",
+                      filename = "inst/extdata/functionhex.png")
+}
